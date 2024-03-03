fca46ece6fb60e3ddd9cf606b177a36aaf15a4de
EchoJamie <myz0104@163.com>
2024-03-03 19:31:53 +0800

    :sparkles: add linux

--- isx.rb
+++ isx.rb
@@ -8,11 +8,43 @@ class Isx < Formula
 
   # depends_on "cmake" => :build
 
+  checksums = {
+    "darwin-arm64" => "0b16fb5bc6579e71a8a186a691e6b6c10b25c1fc339775621d4c34f4e340a1de",
+    # "darwin-intel" => ""
+    "linux-arm64" => "https://github.com/isxcode/isx-cli/releases/download/v1.1.0/isx_linux_amd64",
+    # "linux-intel" => ""
+  }
+
+  on_macos do
+    on_arm do
+      url "https://github.com/isxcode/isx-cli/releases/download/v#{version}/isx_darwin_arm64"
+      sha256 checksums["darwin-arm64"]
+    end
+    # on_intel do
+    #   url "https://github.com/isxcode/isx-cli/releases/download/v#{version}/isx_darwin_intel"
+    #   sha256 checksums["darwin-intel"]
+    # end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/isxcode/isx-cli/releases/download/v#{version}/isx_linux_arm64"
+      sha256 checksums["linux-amd64"]
+    end
+        # on_intel do
+    #   url "https://github.com/isxcode/isx-cli/releases/download/v#{version}/isx_linux_intel"
+    #   sha256 checksums["darwin-intel"]
+    # end
+  end
+
+
   def install
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     # system "./configure", "--disable-silent-rules", *std_configure_args
-    bin.install "isx_darwin_arm64" => "isx"
+    arch = Hardware::CPU.intel? ? "intel" : Hardware::CPU.arch.to_s if OS.mac?
+    kernel = OS.mac? ? "darwin" : "linux"
+    bin.install "isx_#{kernel}_#{arch}" => "isx"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
